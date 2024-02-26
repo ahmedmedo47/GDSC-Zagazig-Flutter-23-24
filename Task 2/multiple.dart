59b425623fb19325457dc99d541196c2c90f464f
ahmed <ahm761171@gmail.com>
2024-02-26 18:06:44 +0200

    Updated

---
+++ Task 2/multiple.dart
@@ -0,0 +1,8 @@
+/*
+1- var myMap={key1:value,key2,value};
+2-(d) All of the above.
+3-(c) To pass data back to the caller when the function finishes.
+4-(c) while loop 
+5-(b) map applies a function to each element, while forEach executes a
+statement
+*/
